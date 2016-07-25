f89ad36affb7029554ddaff5b4fd4d0ac7c41c81
Robert Hernandez <robert.hernandez@breaktech.com>
2016-07-25 14:39:33 -0500

    Added ssh server

--- src/php-5.5/build.sh
+++ src/php-5.5/build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+apt-get update
+apt-get install -y openssh-server
+/etc/init.d/ssh start
+
 puppet apply --modulepath=/src/php-5.5/build/modules /src/php-5.5/build/build.pp
+
+echo -e "container\ncontainer" | passwd container
--- src/php-5.5/run.sh
+++ src/php-5.5/run.sh
@@ -3,3 +3,5 @@
 puppet apply --modulepath=/src/php-5.5/run/modules /src/php-5.5/run/run.pp
 
 supervisord -c /etc/supervisor/supervisord.conf
+
+/etc/init.d/ssh start
